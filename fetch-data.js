fe483493e7daa04dbd5362fe5b3284649fdafe6e
Atikacelestine <celestineatika313@gmail.com>
2024-10-13 22:06:03 +0300

    first commit

---
+++ fetch-data.js
@@ -0,0 +1,25 @@
+//Intialize the Async Function
+async function fetchUserData();{ 
+const apiUrl='https://jsonplaceholder.typicode.com/users'
+    const dataContainer= document.getElementById('api-data')
+    try{
+        dataContainer.innerHTML="";
+        dataContainer.text='Loading user data...';
+        const response = await fetch (apiUrl);
+        const users = await response.json();
+
+        dataContainer.text.Content="";
+        const userList=dociment.createElement('ul');
+        users.forEach(user=>{
+            const listItem =document.createElement('li');
+            listItem.textContent= user.name;
+            userList.appenChild(listItem);
+        });
+        dataContainer.appendChild(userList);
+
+    }catch(error){
+        dataContainer.textContent='Failed to load user data';
+        console.error('Error.',error);
+    }
+}
+document.addEventListener('DOMContentLoaded',fetchUserData);
